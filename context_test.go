48f4366b40a12acfd717edd7969b59de6de3c01d
cino <cmnxx14@gmail.com>
2023-08-10 23:29:33 +0700

    initial commit

--- context_test.go
+++ context_test.go
@@ -3,7 +3,9 @@ package belajar_context
 import (
 	"context"
 	"fmt"
+	"runtime"
 	"testing"
+	"time"
 )
 
 func TestContext(t *testing.T) {
@@ -13,4 +15,94 @@ func TestContext(t *testing.T) {
 	todo := context.TODO()
 	fmt.Println(todo)
 
+}
+
+// Hierarchy of Context
+func TestContextWithValue(t *testing.T) {
+	contextA := context.Background()
+
+	contextB := context.WithValue(contextA, "b", "B")
+	contextC := context.WithValue(contextA, "c", "C")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextB, "e", "E")
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+}
+
+func CreateCounter(ctx context.Context) chan int {
+	destination := make(chan int)
+
+	go func() {
+		defer close(destination)
+		counter := 1
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+			}
+		}
+	}()
+
+	return destination
+}
+
+func TestContextWithCancel(t *testing.T) {
+	fmt.Println("Total Runtime", runtime.NumGoroutine())
+	parent := context.Background()
+	ctx, cancel := context.WithCancel(parent)
+
+	destination := CreateCounter(ctx)
+	for n := range destination {
+		fmt.Println("Counter", n)
+		if n == 10 {
+			break
+		}
+	}
+	cancel() //mengirim sinyal ke cancel
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Total Runtime", runtime.NumGoroutine())
+}
+
+func TestContextWithTimeOut(t *testing.T) {
+	fmt.Println("Total Runtime", runtime.NumGoroutine())
+	parent := context.Background()
+	ctx, cancel := context.WithTimeout(parent, 5*time.Second)
+	defer cancel()
+
+	destination := CreateCounter(ctx)
+	for n := range destination {
+		fmt.Println("Counter", n)
+	}
+	cancel() //mengirim sinyal ke cancel
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Total Runtime", runtime.NumGoroutine())
+}
+
+func TestContextWithDeadline(t *testing.T) {
+	fmt.Println("Total Runtime", runtime.NumGoroutine())
+	parent := context.Background()
+	ctx, cancel := context.WithDeadline(parent, time.Now().Add(5*time.Second))
+	defer cancel()
+
+	destination := CreateCounter(ctx)
+	for n := range destination {
+		fmt.Println("Counter", n)
+	}
+	cancel() //mengirim sinyal ke cancel
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Total Runtime", runtime.NumGoroutine())
 }
